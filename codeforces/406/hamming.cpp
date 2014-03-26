c4c6b784fe6d81b76a20b7042aa2e9d6b62c8c5c
Shashwat <thegreatshasha@gmail.com>
2014-03-26 12:29:51 +0530

    Almost done

--- codeforces/406/hamming.cpp
+++ codeforces/406/hamming.cpp
@@ -13,10 +13,45 @@ int print_triplets(int arr1[], int arr2[], int m) {
 }
 
 int hamming_distance(int bit1, int size1, int bit2, int size2, int n) {
+  //cout << bit1 << " " << size1 << " " << bit2 << " " << size2 << " " << n << endl;
   int diff = abs(size2 - size1);
   int xr = bit1 xor bit2;
 
   return xr * (n - diff) + (not xr) * (diff);
+}
+
+int maximum_hamming(int arr1[], int arr2[], int m, int n) {
+  int currentmax = 0;
+  int currentnum = 1;
+  
+  for (int i = 0; i < m; i++) {
+
+    for (int j = 0; j < m && j != i; j++) {
+
+      for (int k = 0; k < m && k != i && k != j; k++) {
+        int ij_dist = hamming_distance(arr1[i], arr2[i], arr1[j], arr2[j], n);
+        int jk_dist = hamming_distance(arr1[j], arr2[j], arr1[k], arr2[k], n);
+        int ki_dist = hamming_distance(arr1[k], arr2[k], arr1[i], arr2[i], n);
+
+        int distance =  ij_dist + jk_dist + ki_dist;
+        
+        //cout << i << j << "distance: " << ij_dist << endl;
+        //cout << j << k << "distance: " << jk_dist << endl;
+        //cout << k << i << "distance: " << ki_dist << endl;
+        //cout  << "distance: " << distance << endl << endl;
+
+        if (currentmax < distance) {
+          currentmax = distance;
+          currentnum = 1;
+        }
+        else if(currentmax == distance) {
+          currentnum++;
+        }
+      }
+    }
+  }
+  
+  cout << currentnum;
 }
 
 int main() {
@@ -31,8 +66,7 @@ int main() {
   }
 
   //print_triplets(arr1, arr2, m);
-  cout << hamming_distance(0, 3, 1, 4, 5);
-  cout << hamming_distance(0, 3, 0, 5, 5);
+  maximum_hamming(arr1, arr2, m, n);
 }
 
 
