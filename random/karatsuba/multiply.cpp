ba6779e094f84e3bc1737eac50354cc335afd8a0
bhsaurabh <srbhatia10@gmail.com>
2014-05-10 06:09:05 +0530

    almost fuctional

--- random/karatsuba/multiply.cpp
+++ random/karatsuba/multiply.cpp
@@ -5,12 +5,20 @@
 # include <vector>
 # include <assert.h>
 # include <string>
+# include <sstream>
 
 
 using namespace std;
 
 int length(int num) {
   return floor(log10(abs(num))) + 1;
+}
+
+string convertInt(int number)
+{
+   stringstream ss;//create a stringstream
+   ss << number;//add number to the stream
+   return ss.str();//return a string with the contents of the stream
 }
 
 vector<int> num_to_vec(int num) {
@@ -24,33 +32,52 @@ vector<int> num_to_vec(int num) {
   return myint;
 }
 
-int print_vector(vector<int> num) {
+vector<int> pad_zeros(vector<int> num, int length) {
+  if(num.size() < length) {
+    while(num.size() < length) {
+      num.insert(num.begin(), 0);
+    }
+  }
+
+  return num;
+}
+
+string print_vector(vector<int> num, int start, int end) {
   string output ("[");
   
-  for(vector<int>::iterator it = num.begin(); it != num.end(); ++it) {
-    /* std::cout << *it; ... */
-    output = output + std::to_string(*it);
+  /*for(vector<int>::iterator it = num.begin(); it != num.end(); ++it) {
+    output = output + convertInt(*it);
+  }*/
+
+  for(int i = start; i <= end; ++i) {
+    output = output + convertInt(num[i]);
   }
   
-  ouput = output + "]";
+  output = output + "]";
   return output;
 }
 
-int karatsuba(vector<int> num1, vector<int> num2) {
-  int len1 = num1.size();
-  int len2 = num2.size();
-
+int karatsuba(vector<int> num1, vector<int> num2, int num1_start, int num2_start, int num1_end, int num2_end) {
   // no point karatsubaing if anyone is less than 10
   /*if(num1 < 10 || num2 < 10)
     printf("%d and %d\n", num1, num2);
 */
-  int n = max(len1, len2);
+  printf("vector1: %s vector2: %s\n", print_vector(num1, num1_start, num1_end).c_str(), print_vector(num2, num2_start, num2_end).c_str());
+
+  if(num1_start==num1_end && num2_start==num2_end)
+    return 0;
+
+  int n = max( num1_end-num1_start , num2_end - num2_start);
 
   // pad zeros and print vectors
-  printf("%s\n", print_vector(num1));
+  
+  int mid = n/2;
 
-  int mid = ceil((n+1)/2);
+  // call karatsuba again
   printf("n: %d mid: %d\n", n, mid);
+
+  karatsuba(num1, num2, num1_start, num2_start, num1_start+mid, num2_start+mid);
+  karatsuba(num1, num2, num1_start+mid+1, num2_start+mid+1, num1_end, num2_end);
 }
 
 int main() {
@@ -60,9 +87,12 @@ int main() {
   vector<int> num1_vec = num_to_vec(num1);
   vector<int> num2_vec = num_to_vec(num2);
 
-  karatsuba(num1_vec, num2_vec);
-  
-  //print_vector(num1_vec);
+  int n = max( num1_vec.size() , num2_vec.size());
+
+  num1_vec = pad_zeros(num1_vec, n);
+  num2_vec = pad_zeros(num2_vec, n);
+
+  karatsuba(num1_vec, num2_vec, 0, 0, num1_vec.size()-1, num2_vec.size()-1);
   
   // statistical assertion testing here
   //assert(1==2);
