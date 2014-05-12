f20b0a144148160064548f70b0ad095bb4d70db2
bhsaurabh <srbhatia10@gmail.com>
2014-05-12 11:28:05 +0530

    added statistical testing

--- random/karatsuba/multiply.cpp
+++ random/karatsuba/multiply.cpp
@@ -60,9 +60,9 @@ string print_vector(vector<int> num, int start, int end) {
 int karatsuba(vector<int> num1, vector<int> num2, int num1_start, int num2_start, int num1_end, int num2_end) {
   // no point karatsubaing if anyone is less than 10
   /*if(num1 < 10 || num2 < 10)
-    printf("%d and %d\n", num1, num2);
+    //printf("%d and %d\n", num1, num2);
 */
-  printf("vector1: %s vector2: %s\n", print_vector(num1, num1_start, num1_end).c_str(), print_vector(num2, num2_start, num2_end).c_str());
+  //printf("vector1: %s vector2: %s\n", print_vector(num1, num1_start, num1_end).c_str(), print_vector(num2, num2_start, num2_end).c_str());
   // Assumption: num1 and num2 are of equal size
 
   if(num1_start==num1_end && num2_start==num2_end)
@@ -80,24 +80,24 @@ int karatsuba(vector<int> num1, vector<int> num2, int num1_start, int num2_start
   int len = mid;
 
   // call karatsuba again
-  printf("n: %d mid: %d\n", n, mid);
+  //printf("n: %d mid: %d\n", n, mid);
 
   // calculate the third vector. Make this a separate routine
   vector<int> sub1, sub2;
 
   int carry1 = 0, carry2 = 0;
   
-  printf("subvector calc: from: %d to: %d\n", mid, 0);
+  //printf("subvector calc: from: %d to: %d\n", mid, 0);
   for(int i=midleft; i>=0; i--) {
-    printf("  %d corresponds to %d\n", i, i + len);
+    //printf("  %d corresponds to %d\n", i, i + len);
     int n1 = num1[i];
     int n2 = i+len > midleft ? num1[i+len] : 0;
 
     int n3 = num2[i];
     int n4 = i+len > midleft ? num2[i+len] : 0;
     
-    //printf("add: %d + %d \n", n1, n2);
-    //printf("corresponding: \n");
+    ////printf("add: %d + %d \n", n1, n2);
+    ////printf("corresponding: \n");
     
     int rez1 = n1 + n2 + carry1;
     int rez2 = n3 + n4 + carry2;
@@ -121,7 +121,7 @@ int karatsuba(vector<int> num1, vector<int> num2, int num1_start, int num2_start
   sub1 = pad_zeros(sub1, max(sub1.size(), sub2.size()));
   sub2 = pad_zeros(sub2, max(sub1.size(), sub2.size()));
 
-  printf("sub vector1 is: %s, sub vector 2 is: %s\n\n", print_vector(sub1, 0, sub1.size()-1).c_str(), print_vector(sub2, 0, sub2.size()-1).c_str());
+  //printf("sub vector1 is: %s, sub vector 2 is: %s\n\n", print_vector(sub1, 0, sub1.size()-1).c_str(), print_vector(sub2, 0, sub2.size()-1).c_str());
 
 
   //Now calculate the trinity
@@ -136,13 +136,10 @@ int karatsuba(vector<int> num1, vector<int> num2, int num1_start, int num2_start
   
   return result;
 
-  //printf("z1: %d z2: %d\n", z1, z2);
+  ////printf("z1: %d z2: %d\n", z1, z2);
 }
 
-int main() {
-  int num1, num2, result;
-  cin >> num1 >> num2;
-
+int run_karatsuba(int num1, int num2) {
   vector<int> num1_vec = num_to_vec(num1);
   vector<int> num2_vec = num_to_vec(num2);
 
@@ -151,8 +148,31 @@ int main() {
   num1_vec = pad_zeros(num1_vec, n);
   num2_vec = pad_zeros(num2_vec, n);
 
-  result = karatsuba(num1_vec, num2_vec, 0, 0, num1_vec.size()-1, num2_vec.size()-1);
-  printf("final-result; %d\n", result);
+  int result = karatsuba(num1_vec, num2_vec, 0, 0, num1_vec.size()-1, num2_vec.size()-1);
+
+  return result;
+}
+
+int statistical_tests(int n=100) {
+  for(int i=0; i<n; i++) {
+    int num1 = rand() % 1000;
+    int num2 = rand() % 1000;
+
+    int rez1 = run_karatsuba(num1, num2);
+    int rez2 = num1 * num2;
+
+    printf("Testing with num1: %d, num2: %d, rez1: %d, rez2: %d\n", num1, num2, rez1, rez2);
+    assert(rez1==rez2);
+  }
+}
+
+int main() {
+  //int num1, num2, result;
+  //cin >> num1 >> num2;
+
+  statistical_tests(100);
+
+  ////printf("final-result; %d\n", result);
   
   // statistical assertion testing here
   //assert(1==2);
